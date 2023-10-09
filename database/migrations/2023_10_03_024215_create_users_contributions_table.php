0a516cfd59649ad39809edb345dcf256ec487a6c
ElvisAns <ansimapersic@gmail.com>
2023-10-10 00:55:29 +0200

    db & main features

---
+++ database/migrations/2023_10_03_024215_create_users_contributions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_contributions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade'); //note here contrained assume we have users table
+            $table->unsignedBigInteger('montant');
+            $table->boolean('confirmed')->default(false);
+            $table->string('payment_method_used')->default("-");
+            $table->string('paid_to_tel')->default("-");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_contributions');
+    }
+};

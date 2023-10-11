cdc7d6a44f14de526bbe6c1e024bbc011939c60d
ElvisAns <ansimapersic@gmail.com>
2023-10-11 05:09:17 +0200

    fix case issue on namespace

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
-use App\models\User;
+use App\Models\User;
 
 class Welcome extends Controller
 {
